76278b04442042a8576d60721d61c258e0142cd3
mishana <websoullabsmishana@gmail.com>
2022-09-29 11:00:34 +0530

    committing detailed page

--- src/components/Consultantdetail/Consultantdetail.js
+++ src/components/Consultantdetail/Consultantdetail.js
@@ -1,11 +1,26 @@
-import React from 'react'
+import * as React from 'react';
+import Box from '@mui/material/Box';
+import Paper from '@mui/material/Paper';
 
-function Consultantdetail() {
+export default function SimplePaper() {
   return (
-    <div>
-      working
+    <div className='section'>
+   <Box
+      sx={{
+        display: 'flex',
+        flexWrap: 'wrap',
+        '& > :not(style)': {
+          m: 2,
+          width: 700,
+          height: 300,
+        },
+      }}
+    >
+      <Paper elevation={0} />
+      <Paper />
+     
+    </Box>
     </div>
-  )
-}
 
-export default Consultantdetail
+  );
+}
