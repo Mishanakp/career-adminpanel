ccf4c663387e58f0f18b8cfda0f90d675798cc24
mishana <websoullabsmishana@gmail.com>
2022-09-29 23:22:17 +0530

    commitingg

--- src/components/Consultantdetail/Consultantdetail.js
+++ src/components/Consultantdetail/Consultantdetail.js
@@ -1,26 +1,35 @@
 import * as React from 'react';
+import { styled } from '@mui/material/styles';
 import Box from '@mui/material/Box';
 import Paper from '@mui/material/Paper';
+import Grid from '@mui/material/Grid';
+import './Consultantdetail.css';
 
-export default function SimplePaper() {
+const Item = styled(Paper)(({ theme }) => ({
+  backgroundColor: theme.palette.mode === 'dark' ? '#1A2027' : '#fff',
+  ...theme.typography.body2,
+  padding: theme.spacing(1),
+  textAlign: 'center',
+  color: theme.palette.text.secondary,
+}));
+
+export default function AutoGrid() {
   return (
-    <div className='section'>
-   <Box
-      sx={{
-        display: 'flex',
-        flexWrap: 'wrap',
-        '& > :not(style)': {
-          m: 2,
-          width: 700,
-          height: 300,
-        },
-      }}
-    >
-      <Paper elevation={0} />
-      <Paper />
-     
+    <div className='grid'>
+    <Box sx={{ flexGrow: 1 }}>
+      <Grid container spacing={3}>
+        <Grid item xs={4}></Grid>
+        <Grid item xl={4} lg={4} md={4} sm={4} xs={4}>
+          <Item>
+              <img className='imageone' src="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcRJ6Yf7nJpw2TLXEnKYwgtQNWYLcaeO0Kiw7Q&usqp=CAU"></img>
+              <h4 className='headone'>McKinsey</h4>
+              <h5 className='headpara'>McKinsey@gmail.com</h5>
+              <h5 className='headnum'>+04679754435</h5>
+          </Item>
+        </Grid>
+        <Grid item xs></Grid>
+      </Grid>
     </Box>
     </div>
-
   );
 }
